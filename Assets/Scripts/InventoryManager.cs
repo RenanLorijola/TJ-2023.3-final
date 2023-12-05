efe18337a6542615a7043b0e44386197a8f8f70d
Carlos Junior <casjunior.95@gmail.com>
2023-12-05 12:29:18 -0300

    feat: inventário inicial

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventoryManager : MonoBehaviour
+{
+    public Transform ItemContent;
+    public GameObject InventoryItem;
+    private List<Equipment> equipments = new List<Equipment>();
+    public void ListItems()
+    {
+        equipments = PlayerManager.Singleton.GetEquipments();
+
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+
+        foreach (var item in equipments)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<Text>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+
+            itemName.text = item.EquipmentKey;
+            itemIcon.sprite = item.Icon;
+            
+        }
+    }
+}
