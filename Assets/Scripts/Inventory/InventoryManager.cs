6d58e888cb063b230eafd191631d4ba19b5e8628
FusiDaniel <fpires.daniel@gmail.com>
2023-12-07 11:38:18 -0300

    conserta inverntário na floresta

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class InventoryManager : MonoBehaviour
 {
@@ -18,16 +19,13 @@ public class InventoryManager : MonoBehaviour
 
         foreach (var item in equipments)
         {
-            int grabbed_knife = GameManager.Singleton.GetFlag("grabbed_knife");
-            int grabbed_gun = GameManager.Singleton.GetFlag("grabbed_gun");
+            bool grabbed_knife = GameManager.Singleton.GetFlag("grabbed_knife") != 0 || SceneManager.GetActiveScene().name == "Floresta";
+            bool grabbed_gun = GameManager.Singleton.GetFlag("grabbed_gun") != 0 || SceneManager.GetActiveScene().name == "Floresta";
 
-            if ((item.name.ToLower() == "knife" && grabbed_knife == 1) ||
-                (item.name.ToLower() == "gun" && grabbed_gun == 1))
+            if ((item.name.ToLower() == "knife" && grabbed_knife) ||
+                (item.name.ToLower() == "gun" && grabbed_gun))
             {
-                if (!item.isActiveAndEnabled)
-                {
-                    ShowInventory(item);
-                }
+                ShowInventory(item);
             }      
         }
     }
